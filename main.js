224c8cf73f8ae16bdfc6c8881d2a683d6975a239
wanglifa <39861123+wanglifa@users.noreply.github.com>
2018-07-31 22:28:39 +0800

    Promise
    Promise

--- main.js
+++ main.js
@@ -4,46 +4,34 @@ window.jQuery = function(nodeOrSelector){
     nodes.html = function(){}
     return nodes;
 }
+window.Promise = function(){
+    return {
+        then:function(){};
+    }
+}
 
 window.jQuery.ajax = function({url,method,body,successFn,failFn,header}){
-    
-    
-     
-    //ES6解构赋值
-    //var o = {p: 42, q: true};
-    //var {p, q} = o;
-    //console.log(p); // 42
-    //console.log(q); // true
-    
-    //上面的options就相当于o，他等于你传入的对象{url:'/xxx',method:'post'...},
-    //所以url就是options.url
-    //所以上面的代码就等同于
-    // var url = options.url;
-    // var method = options.method;
-    // var body = options.body;
-    // var header = options.header;
-    // var successFn = options.successFn;
-    // var failFn = options.failFn;
-    
-
-    var request = new XMLHttpRequest;
-    request.onreadystatechange = function(){
-        if(request.readyState === 4){
-            if(request.status>=200 && request.status <300){
-               successFn.call(undefined,request.responseText)
-            }else if(request.status >=400){
-                failFn.call(undefined,request.responseText)
+   return new Promise(function(resolve,reject){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(request.readyState === 4){
+                if(request.status >=200 && request.status <300){
+                    //这里设置then里面第一个匿名函数的实参
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    //这里设置then里面第二个匿名函数的实参
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-    request.open(method,url)
-    for(var key in header){
-        var value = header[key];
-        request.setRequestHeader(key,value);
-    }
-    request.setRequestHeader('wanglifa','18')
-    request.setRequestHeader('Content-Type','x-www-form-urlencoded')
-    request.send(body);
+
+        request.open(method,url)
+        for(var key in header){
+            var value = header[key];
+            request.setRequestHeader(key,value)
+        }
+        request.send(body);
+    }) 
 }
 window.$ = window.jQuery;
 
@@ -51,20 +39,20 @@ function f1(responseText){}
 function f2(responseText){}
 
 myButton.addEventListener('click',(e)=>{
-    window.jQuery.ajax({
-        url:'/xxx',
-        method:'post',
-        body:'这是请求的第四部分内容',
-        header:{
-            'Content-Type':'application/x-www-form-urlencoded',
-            'wanglifa':'18'
-        },
-        successFn: (respon)=>{
-            console.log(respon);
-            f1.call(undefined,x);
-            f2.call(undefined,x);
-        },
-        failFn: (request)=>{console.log(request)}
-    })
+   var promise =window.jQuery.ajax({
+       url:'/xxx',
+       method:'post',
+       body:'这是第四部分',
+       header:{
+           'Content-Type':'application/x-www-form-urlencoded',
+           'wanglifa':'18'
+       }
+   })
+   promise.then(
+       //对应resolve这个函数,这个text就是上面传入的实参request.responseText
+       (text)=>{console.log(text)},
+       //对应reject这个函数,这个b对应上面传入的实参rquest
+       (b)=>{console.log(b)}
+   )
     
 })
